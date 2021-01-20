0abd2c234a4a64f9555df88def6baeb6874c4264
OberusPing <54815158+OberusPing@users.noreply.github.com>
2021-01-20 08:57:25 -0500

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -7,31 +7,6 @@ import java.util.Map.Entry;
 
 public class Sorting {
 
-	/*
-	 * This method takes as input an HashMap with values that are Comparable. 
-	 * It returns an ArrayList containing all the keys from the map, ordered 
-	 * in descending order based on the values they mapped to. 
-	 * 
-	 * The time complexity for this method is O(n^2) as it uses bubble sort, where n is the number 
-	 * of pairs in the map. 
-	 */
-    public static <K, V extends Comparable> ArrayList<K> slowSort (HashMap<K, V> results) {
-        ArrayList<K> sortedUrls = new ArrayList<K>();
-        sortedUrls.addAll(results.keySet());	//Start with unsorted list of urls
-
-        int N = sortedUrls.size();
-        for(int i=0; i<N-1; i++){
-			for(int j=0; j<N-i-1; j++){
-				if(results.get(sortedUrls.get(j)).compareTo(results.get(sortedUrls.get(j+1))) <0){
-					K temp = sortedUrls.get(j);
-					sortedUrls.set(j, sortedUrls.get(j+1));
-					sortedUrls.set(j+1, temp);					
-				}
-			}
-        }
-        return sortedUrls;                    
-    }
-    
     
 	/*
 	 * This method takes as input an HashMap with values that are Comparable. 
@@ -72,21 +47,12 @@ public class Sorting {
 
 		int size = (end+1 - beg);
 
-		//incorporating InsertionSort for smaller lists
-		//if (size <= 10){
-		//	insertionSort(elements, beg, end);
-		//}
-
-		//else use QuickSort for larger lists
-		//else {
 			//update the pivot and swap appropriate elements using the partition helper method
 			int pivot = partition(elements, beg, end);
 
 			//recursively call quicksort on either side of the pivot
 			quicksort(elements, beg, pivot - 1);
 			quicksort(elements, pivot + 1, end);
-		//}
-		//}
 	}
 
 	private static <K extends Comparable<K>> int partition(ArrayList<K> elements, int beg, int end){
@@ -118,32 +84,6 @@ public class Sorting {
 		return beg;
 	}
 
-	/*private static <K extends Comparable<K>> void insertionSort(ArrayList<K> elements, int beg, int end){
-		for (int i = beg + 1; i <= end; i++){
-			K currentElement = elements.get(i);
-
-			while (i >= beg && elements.get(i).compareTo(currentElement) < 0){
-				elements.set(i+1, elements.get(i));
-				i--;
-			}
-			elements.set(i+1,currentElement);
-		}*/
-
-	/*public static int getMedian(int left,int right){
-		int center = (left+right)/2;
-
-		if(a[left] > a[center])
-			swap(left,center);
-
-		if(a[left] > a[right])
-			swap(left, right);
-
-		if(a[center] > a[right])
-			swap(center, right);
-
-		swap(center, right);
-		return a[right];
-	}*/
 	}
 
 
